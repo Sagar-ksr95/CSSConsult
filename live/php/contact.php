92a4ccea114c9427a94800c9642ffcae12e47e89
anirudh74 <anirudhkonduri@anirudhkondurithoughtworkscom.local>
2023-08-15 21:40:21 +0530

    Added code for send mail

--- live/php/contact.php
+++ live/php/contact.php
@@ -6,43 +6,6 @@
 	$apiKeyPublic = getenv('MJ_APIKEY_PUBLIC');
 	$apiKeyPrivate = getenv('MJ_APIKEY_PRIVATE');
 
-	// Check if name has been entered
-	if (!isset($_POST['name'])) {
-		$errors['name'] = 'Please enter your name';
-	}
-
-	// Check if email has been entered and is valid
-	if (!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-		$errors['email'] = 'Please enter a valid email address';
-	}
-
-	//Check if message has been entered
-	if (!isset($_POST['message'])) {
-		$errors['message'] = 'Please enter your message';
-	}
-
-	$errorOutput = '';
-
-	// if(!empty($errors)){
-
-	// 	$errorOutput .= '<div class="alert alert-danger alert-dismissible" role="alert">';
- 	// 	$errorOutput .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
-
-	// 	$errorOutput  .= '<ul>';
-
-	// 	foreach ($errors as $key => $value) {
-	// 		$errorOutput .= '<li>'.$value.'</li>';
-	// 	}
-
-	// 	$errorOutput .= '</ul>';
-	// 	$errorOutput .= '</div>';
-
-	// 	echo $errorOutput;
-	// 	die();
-	// }
-
-
-
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$message = $_POST['message'];
@@ -57,7 +20,7 @@
 
 	//send the email
 	$result = '';
-	// if (mail ($to, $subject, $body, $headers)) {
+	/*// if (mail ($to, $subject, $body, $headers)) {
 	// 	$result .= '<div class="alert alert-success alert-dismissible" role="alert">';
  	// 	$result .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
 	// 	$result .= 'Thank You! I will be in touch';
@@ -73,7 +36,7 @@
 	// $result .= 'Something bad happend during sending this message. Please try again later';
 	// $result .= '</div>';
 
-	// echo $result;
+	// echo $result;*/
 	if(sendMailjetEmail($apiKeyPublic, $apiKeyPrivate, $recipientEmail,$name,$email,$message)) {
 		/*
 		This call sends a message to the given recipient with vars and custom vars.
